082d8450e15ebe54720b66c4a7e6d548a9d275a4
Sanya, The Creator <olexxxandr.romaniuk@gmail.com>
2026-03-24 17:29:34 +0100

    example of using templates

--- main.go
+++ main.go
@@ -3,15 +3,35 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 const portNumber = ":8080"
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world")
+	const fn = "home.page.tmpl"
+	renderTemplate(w, fn)
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	const fn = "about.page.tmpl"
+	renderTemplate(w, fn)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+
+	if err != nil {
+		fmt.Println("error parsing template: ", err)
+		return
+	}
 }
 
 func main() {
 	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Printf("Starting app on port %s", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
 }
