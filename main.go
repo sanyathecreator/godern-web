de9161b55a256f56754a1b6972a5e11d93085a12
Sanya, The Creator <olexxxandr.romaniuk@gmail.com>
2026-03-24 17:27:51 +0100

    a simple hello world app

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+const portNumber = ":8080"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world")
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	_ = http.ListenAndServe(portNumber, nil)
 }
